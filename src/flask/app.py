c39b81db38fd57cb1832d2924104f660249607fd
Sam Andrew <samandrew27@gmail.com>
2021-06-13 13:54:50 -0600

    minor flask adjustment

--- src/flask/app.py
+++ src/flask/app.py
@@ -2,8 +2,6 @@
 # available here: https://getbootstrap.com/docs/3.3/getting-started/#examples
 
 from flask import Flask, render_template, request
-import numpy as np 
-import pandas as pd 
 
 app = Flask(__name__)
 
